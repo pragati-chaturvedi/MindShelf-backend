3cdd306d650927665bcaa14bb3cafb8e92ff6d9c
Pragati Chaturvedi <pragatichaturvedi@Pragatis-MacBook-Pro-5.local>
2025-07-11 16:23:47 -0700

    Edits to DBConnection logic

--- helpers/dbConnection.js
+++ helpers/dbConnection.js
@@ -1,6 +1,5 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://admin:Qwerty123@mindshelfdb.4wyigrv.mongodb.net/?appName=mindShelfDB";
+const uri = MONGODB_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
