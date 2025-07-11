49c568f7783708f830fa4ff64a2da9d7365181a1
Pragati Chaturvedi <pragatichaturvedi@Pragatis-MacBook-Pro-5.local>
2025-07-11 13:26:27 -0700

    Use FIREBASE_CONFIG from .env instead of JSON file

--- helpers/firebaseAdmin.js
+++ helpers/firebaseAdmin.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../firebaseServiceAccount.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
