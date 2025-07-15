f5044e373d714cf9addc8f94341fd18564fc7267
Pragati Chaturvedi <pragatichaturvedi@Pragatis-MacBook-Pro-5.local>
2025-07-14 22:44:46 -0700

    Fixed Error

--- helpers/dbConnection.js
+++ helpers/dbConnection.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = MONGODB_URI;
+const uri = process.env.MONGODB_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
